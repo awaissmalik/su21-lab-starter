2890c9eabb353c4a0f75afce5ac957ca8c2c2f26
awaissmalik <140106268+awaissmalik@users.noreply.github.com>
2023-11-29 17:21:20 +0500

    Exercise 4 of Lab01 has been completed

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,23 @@
 #include <stddef.h>
 #include "ll_cycle.h"
-
+#include <stdbool.h>
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if (head == NULL) {
+        return false; // An empty list cannot have a cycle.
+    }
+
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+
+    while (fast_ptr != NULL && fast_ptr->next != NULL) {
+        fast_ptr = fast_ptr->next->next; // Advance fast_ptr by two nodes
+        slow_ptr = slow_ptr->next;       // Advance slow_ptr by one node
+
+        if (fast_ptr == slow_ptr) {
+            return true; // fast_ptr and slow_ptr have met, indicating a cycle
+        }
+    }
+
+    return false; // Reached the end of the list, no cycle found
 }
