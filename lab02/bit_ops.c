02aa6ac15ae867eab5a635553fa530a57c048747
awaissmalik <140106268+awaissmalik@users.noreply.github.com>
2023-12-03 14:27:53 +0500

    Update bit_ops.c

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "bit_ops.h"
+
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
     // Create bit with the Nth bit set to 1
@@ -11,15 +12,36 @@ unsigned get_bit(unsigned x, unsigned n) {
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
 void set_bit(unsigned *x, unsigned n, unsigned v) {
-	unsigned bit = ( v << n ) ;
-	*x = ( *x & (~(1 << n ))) | bit;
-
+    unsigned bit = (v << n);
+    *x = (*x & (~(1 << n))) | bit;
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
     // Create a bit with only the Nth bit set to 1
-    unsigned bit = 1 << n;   
+    unsigned bit = 1 << n;
     // Use bit XOR to flip the Nth bit (0 to 1 or 1 to 0)
     *x = (*x) ^ bit;
+}
+
+int main() {
+    // Example usage of functions
+    unsigned x = 42; // Or any other value
+    unsigned n = 2;  // Or any other bit position
+    unsigned v = 1;  // Or 0 to clear the bit
+
+    printf("Original value: %u\n", x);
+
+    // Test get_bit
+    printf("Bit at position %u: %u\n", n, get_bit(x, n));
+
+    // Test set_bit
+    set_bit(&x, n, v);
+    printf("After setting bit %u to %u: %u\n", n, v, x);
+
+    // Test flip_bit
+    flip_bit(&x, n);
+    printf("After flipping bit %u: %u\n", n, x);
+
+    return 0;
 }
