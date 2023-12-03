c0d5cda62c4e2af7b100f00bbd95df1c4798cc81
awaissmalik <awaissmalik.1@gmail.com>
2023-12-03 14:16:50 +0500

    Lab02_completed

--- lab02/linked_list.c
+++ lab02/linked_list.c
@@ -39,7 +39,7 @@ void add_to_front(struct Node **head, int data) {
     *head = new_node;
     /* The following line would not work because it would only change our local copy of HEAD */
     /* head = new_node */
- Printf("This is the change in the program");
+ printf("This is the change in the program");
 }
 
 /* Prints out a linked list starting at HEAD */
