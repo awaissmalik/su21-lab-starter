82fc97671a7da9540ae94eb449e9c9d9011d688c
awaissmalik <140106268+awaissmalik@users.noreply.github.com>
2023-12-03 14:18:04 +0500

    Update bit_ops.c

--- lab02/bit_ops.c
+++ lab02/bit_ops.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include "bit_ops.h"
-
 /* Returns the Nth bit of X. Assumes 0 <= N <= 31. */
 unsigned get_bit(unsigned x, unsigned n) {
-    /* YOUR CODE HERE */
-    return -1; /* UPDATE WITH THE CORRECT RETURN VALUE*/
+    // Create bit with the Nth bit set to 1
+    unsigned bit = 1 << n;
+    // Use bit AND to check if the Nth bit is set
+    unsigned result = (x & bit) >> n;
+    return result;
 }
 
 /* Set the nth bit of the value of x to v. Assumes 0 <= N <= 31, and V is 0 or 1 */
 void set_bit(unsigned *x, unsigned n, unsigned v) {
-    /* YOUR CODE HERE */
+	unsigned bit = ( v << n ) ;
+	*x = ( *x & (~(1 << n ))) | bit;
+
 }
 
 /* Flips the Nth bit in X. Assumes 0 <= N <= 31.*/
 void flip_bit(unsigned *x, unsigned n) {
-    /* YOUR CODE HERE */
+    // Create a bit with only the Nth bit set to 1
+    unsigned bit = 1 << n;   
+    // Use bit XOR to flip the Nth bit (0 to 1 or 1 to 0)
+    *x = (*x) ^ bit;
 }
-
